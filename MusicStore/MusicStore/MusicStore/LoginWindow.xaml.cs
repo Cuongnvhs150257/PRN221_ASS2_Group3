ec6c0c57998648de2d4ccfdea4c22bc1da7596a6
AsheS <campus265@gmail.com>
2023-02-16 21:52:59 +0700

    Update LoginWindow.xaml.cs

--- MusicStore/MusicStore/MusicStore/LoginWindow.xaml.cs
+++ MusicStore/MusicStore/MusicStore/LoginWindow.xaml.cs
@@ -42,7 +42,7 @@ namespace MusicStore
             }
             else
             {
-                MessageBox.Show("Success hai mặt heo!!! ");
+                MessageBox.Show("Trường mặc cứt");
 
                 this.Close();
             }
