d32620739529585ef408aa271c11048d11abf277
AsheS <campus265@gmail.com>
2023-02-18 23:07:11 +0700

    Update Shoping Window

--- MusicStore/MusicStore/MusicStore/ShoppingWindow.xaml.cs
+++ MusicStore/MusicStore/MusicStore/ShoppingWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MusicStore.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using MusicStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,15 +22,35 @@ namespace MusicStore
     public partial class ShoppingWindow : Window
     {
         MusicStoreContext context = new MusicStoreContext();
+        int previous = 0;
+        int next = 0;
         public ShoppingWindow()
         {
             InitializeComponent();
             cbGenre.ItemsSource = context.Genres.ToList().Select(genre => genre.Name).ToList();
+            next = previous + 1;
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            var query = context.Albums.OrderBy(ab => ab.AlbumId);
+            var paginatedList = await PaginatedList<Album>.CreateAsync(query, 1, 4);
+            lvAlbums.ItemsSource = paginatedList;
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            cbGenre.ItemsSource = context.Genres.ToList().Select(genre => genre.Name).ToList();
+
+        }
+
+        private void previousPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nextPage_Click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
