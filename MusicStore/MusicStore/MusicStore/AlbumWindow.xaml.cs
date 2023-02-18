52afedd2f9a7073ca32341af4377f4f57b2797de
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2023-02-18 21:00:01 +0700

    add backend for view album
    CRUD

--- MusicStore/MusicStore/MusicStore/AlbumWindow.xaml.cs
+++ MusicStore/MusicStore/MusicStore/AlbumWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,117 @@ namespace MusicStore
     /// </summary>
     public partial class AlbumWindow : Window
     {
+
+        MusicStoreContext context = new MusicStoreContext();
+
         public AlbumWindow()
         {
             InitializeComponent();
+            lvAlbum.ItemsSource = context.Albums.ToList();
+            cbGenre.ItemsSource = context.Genres.ToList().Select(name => name.Name).ToList();
+            cbArtist.ItemsSource = context.Artists.ToList().Select(name => name.Name).ToList();
+        }
+
+        private void cbGenre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var album = lvAlbum.SelectedItem as Album;
+            if (album != null)
+            {
+                MessageBoxResult messageResult = System.Windows.MessageBox.Show("Are you sure?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+                if (messageResult == MessageBoxResult.No)
+                {
+                    return;
+                }
+                try
+                {
+                    context.Albums.Remove(album);
+                    if (context.SaveChanges() > 0)
+                    {
+                        MessageBox.Show($"{album.Title} Delete success");
+                        LoadAlbum();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void LoadAlbum()
+        {
+            lvAlbum.ItemsSource = context.Albums.ToList();
+            cbGenre.ItemsSource = context.Genres.ToList().Select(name => name.Name).ToList();
+            cbArtist.ItemsSource = context.Artists.ToList().Select(name => name.Name).ToList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Genre genre = context.Genres.SingleOrDefault(ge => ge.Name == cbGenre.Text);
+                Artist artist = context.Artists.SingleOrDefault(co => co.Name == cbArtist.Text);
+                Album album = new Album
+                {
+
+                    GenreId = genre.GenreId,
+                    Title = txtTitle.Text,
+                    Price = decimal.Parse(txtPrice.Text),
+                    AlbumUrl = txtUrl.Text,
+                    ArtistId = artist.ArtistId
+
+                };
+                context.Albums.Add(album);
+                int count = context.SaveChanges();
+                if (count > 0)
+                {
+                    MessageBox.Show("Insert success");
+                    LoadAlbum();
+                }
+                else
+                {
+                    MessageBox.Show("Insert failure");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Insert error: " + ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Genre genre = context.Genres.SingleOrDefault(ge => ge.Name == cbGenre.Text);
+                Artist artist = context.Artists.SingleOrDefault(co => co.Name == cbArtist.Text);
+                var album = lvAlbum.SelectedItem as Album;
+                if (album != null)
+                {
+                    album.Title = txtTitle.Text;
+                    album.GenreId = genre.GenreId;
+                    album.ArtistId = artist.ArtistId;
+                    album.Price = decimal.Parse(txtPrice.Text);
+                    album.AlbumUrl = txtUrl.Text;
+                    context.Albums.Update(album);
+                    if (context.SaveChanges() > 0)
+                    {
+                        MessageBox.Show($"{album.Title} Update success");
+                        LoadAlbum();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Insert error: " + ex.Message);
+            }
         }
     }
+    
 }
